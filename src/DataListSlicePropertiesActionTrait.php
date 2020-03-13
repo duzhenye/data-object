2103caf2498a661d3db1424de70d234bc12a27e7
duzhenye <60967905+duzhenye@users.noreply.github.com>
2020-03-13 09:52:47 +0800

    Create DataListSlicePropertiesActionTrait.php

---
+++ src/DataListSlicePropertiesActionTrait.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * Data Object
+ * https://github.com/duzhenye/data-object
+ * Copyright (c) duzhenye
+ * Free to use under the MIT license.
+ */
+
+namespace Duzhenye;
+
+/**
+ *
+ */
+trait DataListSlicePropertiesActionTrait
+{
+
+    /**
+     * The properties list.
+     * 
+     * @var array
+     */
+    public $properties = null;
+}
