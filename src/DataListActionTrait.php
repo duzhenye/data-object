c6472ee80f08397c9a47a73e9dd8cc7988aa5c31
duzhenye <60967905+duzhenye@users.noreply.github.com>
2020-03-13 09:47:55 +0800

    Create DataListActionTrait.php

---
+++ src/DataListActionTrait.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * Data Object
+ * https://github.com/duzhenye/data-object
+ * Copyright (c) duzhenye
+ * Free to use under the MIT license.
+ */
+
+namespace Duzhenye;
+
+/**
+ * 
+ */
+trait DataListActionTrait
+{
+
+    /**
+     * The name of the action.
+     * 
+     * @var string
+     */
+    public $name = null;
+}
