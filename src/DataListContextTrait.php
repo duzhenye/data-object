303813f33a0748221ce5121e39a1f647c307399a
duzhenye <60967905+duzhenye@users.noreply.github.com>
2020-03-13 09:49:38 +0800

    Create DataListContextTrait.php

---
+++ src/DataListContextTrait.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * Data Object
+ * https://github.com/duzhenye/data-object
+ * Copyright (c) duzhenye
+ * Free to use under the MIT license.
+ */
+
+namespace Duzhenye;
+
+/**
+ *
+ */
+trait DataListContextTrait
+{
+
+    /**
+     * An array containing all the actions.
+     * 
+     * @var array 
+     */
+    public $actions;
+}
