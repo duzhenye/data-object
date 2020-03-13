944f1b63603c6cd2d7194d60f0391c56f26f1b38
duzhenye <60967905+duzhenye@users.noreply.github.com>
2020-03-13 09:52:26 +0800

    Create DataListSlicePropertiesAction.php

---
+++ src/DataListSlicePropertiesAction.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * Data Object
+ * https://github.com/duzhenye/data-object
+ * Copyright (c) duzhenye
+ * Free to use under the MIT license.
+ */
+
+namespace Duzhenye;
+
+/**
+ * Information about a sliceProperties action applied on a data list.
+ */
+class DataListSlicePropertiesAction extends \Duzhenye\DataListAction
+{
+
+    use \Duzhenye\DataListSlicePropertiesActionTrait;
+}
