80b0d31293c64515b8cedec01f81712112c83563
duzhenye <60967905+duzhenye@users.noreply.github.com>
2020-03-13 09:50:03 +0800

    Create DataListFilterByAction.php

---
+++ src/DataListFilterByAction.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * Data Object
+ * https://github.com/duzhenye/data-object
+ * Copyright (c) duzhenye
+ * Free to use under the MIT license.
+ */
+
+namespace Duzhenye;
+
+/**
+ * Information about a filterBy action applied on a data list.
+ */
+class DataListFilterByAction extends \Duzhenye\DataListAction
+{
+
+    use \Duzhenye\DataListFilterByActionTrait;
+}
