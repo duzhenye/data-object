a5ef55ecfc86fe088ddf35e7fbdfb8eb3375b345
duzhenye <60967905+duzhenye@users.noreply.github.com>
2020-03-13 09:51:59 +0800

    Create DataListSliceActionTrait.php

---
+++ src/DataListSliceActionTrait.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * Data Object
+ * https://github.com/duzhenye/data-object
+ * Copyright (c) duzhenye
+ * Free to use under the MIT license.
+ */
+
+namespace Duzhenye;
+
+/**
+ *
+ */
+trait DataListSliceActionTrait
+{
+
+    /**
+     * The offset.
+     * 
+     * @var int
+     */
+    public $offset = null;
+
+    /**
+     * The limit.
+     * 
+     * @var int
+     */
+    public $limit = null;
+}
