d77a49da3c7ffaa9465b5c64ad13957a9fe592cc
duzhenye <60967905+duzhenye@users.noreply.github.com>
2020-03-13 09:57:30 +0800

    Create DataObjectArrayAccessTrait.php

---
+++ src/DataObjectArrayAccessTrait.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+ * Data Object
+ * https://github.com/duzhenye/data-object
+ * Copyright (c) duzhenye
+ * Free to use under the MIT license.
+ */
+
+namespace Duzhenye;
+
+/**
+ * 
+ */
+trait DataObjectArrayAccessTrait
+{
+
+    /**
+     * 
+     * @param string $offset
+     * @return mixed
+     */
+    public function &offsetGet($offset)
+    {
+        return $this->$offset;
+    }
+
+    /**
+     * 
+     * @param string $offset
+     * @param mixed $value
+     */
+    public function offsetSet($offset, $value): void
+    {
+        if ($offset !== null) {
+            $this->$offset = $value;
+        }
+    }
+
+    /**
+     * 
+     * @param string $offset
+     * @return boolean
+     */
+    public function offsetExists($offset): bool
+    {
+        return isset($this->$offset);
+    }
+
+    /**
+     * 
+     * @param string $offset
+     */
+    public function offsetUnset($offset): void
+    {
+        unset($this->$offset);
+    }
+}
