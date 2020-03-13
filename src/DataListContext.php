1ddb70cc307d3f24588cdef013caaa44e1ec23ca
duzhenye <60967905+duzhenye@users.noreply.github.com>
2020-03-13 09:49:12 +0800

    Create DataListContext.php

---
+++ src/DataListContext.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * Data Object
+ * https://github.com/duzhenye/data-object
+ * Copyright (c) duzhenye
+ * Free to use under the MIT license.
+ */
+
+namespace Duzhenye;
+
+/**
+ * Information about the actions applied on a data list.
+ */
+class DataListContext
+{
+
+    use \Duzhenye\DataListContextTrait;
+}
