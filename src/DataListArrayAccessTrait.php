8843dd55a865a5c8d3cb218a3d740a9880b6ad40
duzhenye <60967905+duzhenye@users.noreply.github.com>
2020-03-13 09:48:34 +0800

    Create DataListArrayAccessTrait.php

---
+++ src/DataListArrayAccessTrait.php
@@ -0,0 +1,83 @@
+<?php
+
+/*
+ * Data Object
+ * https://github.com/duzhenye/data-object
+ * Copyright (c) duzhenye
+ * Free to use under the MIT license.
+ */
+
+namespace Duzhenye;
+
+/**
+ * 
+ */
+trait DataListArrayAccessTrait
+{
+
+    /**
+     * 
+     * @param int $offset
+     * @return \Duzhenye\DataObject|null
+     * @throws \InvalidArgumentException
+     */
+    public function offsetGet($offset)
+    {
+        $this->internalDataListUpdate();
+        if (isset($this->internalDataListData[$offset])) {
+            return $this->internalDataListUpdateValueIfNeeded($this->internalDataListData, $offset);
+        }
+        return null;
+    }
+
+    /**
+     * 
+     * @param int $offset
+     * @param \Duzhenye\DataObject|null $value
+     * @return void
+     * @throws \InvalidArgumentException
+     * @throws \Exception
+     */
+    public function offsetSet($offset, $value): void
+    {
+        if (!is_int($offset) && $offset !== null) {
+            throw new \Exception('The offset must be of type int or null');
+        }
+        $this->internalDataListUpdate();
+        if (is_null($offset)) {
+            $this->internalDataListData[] = $value;
+            return;
+        }
+        if (is_int($offset) && $offset >= 0 && (isset($this->internalDataListData[$offset]) || $offset === sizeof($this->internalDataListData))) {
+            $this->internalDataListData[$offset] = $value;
+            return;
+        }
+        throw new \Exception('The offset is not valid.');
+    }
+
+    /**
+     * 
+     * @param int $offset
+     * @return boolean
+     * @throws \InvalidArgumentException
+     */
+    public function offsetExists($offset): bool
+    {
+        $this->internalDataListUpdate();
+        return isset($this->internalDataListData[$offset]);
+    }
+
+    /**
+     * 
+     * @param int $offset
+     * @throws \InvalidArgumentException
+     */
+    public function offsetUnset($offset): void
+    {
+        $this->internalDataListUpdate();
+        if (isset($this->internalDataListData[$offset])) {
+            unset($this->internalDataListData[$offset]);
+            $this->internalDataListData = array_values($this->internalDataListData);
+        }
+    }
+}
