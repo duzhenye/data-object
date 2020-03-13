e4292dc8f96a71b9a2c0ef417f4d52081340c027
duzhenye <60967905+duzhenye@users.noreply.github.com>
2020-03-13 09:53:05 +0800

    Create DataListSortByAction.php

---
+++ src/DataListSortByAction.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * Data Object
+ * https://github.com/duzhenye/data-object
+ * Copyright (c) duzhenye
+ * Free to use under the MIT license.
+ */
+
+namespace Duzhenye;
+
+/**
+ * Information about a sortBy action applied on a data list.
+ */
+class DataListSortByAction extends \Duzhenye\DataListAction
+{
+
+    use \Duzhenye\DataListSortByActionTrait;
+}
