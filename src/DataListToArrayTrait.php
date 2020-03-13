65a422905b5fe9e8163b18303dcc521dbf29e966
duzhenye <60967905+duzhenye@users.noreply.github.com>
2020-03-13 09:55:23 +0800

    Create DataListToArrayTrait.php

---
+++ src/DataListToArrayTrait.php
@@ -0,0 +1,93 @@
+<?php
+
+/*
+ * Data Object
+ * https://github.com/duzhenye/data-object
+ * Copyright (c) duzhenye
+ * Free to use under the MIT license.
+ */
+
+namespace Duzhenye;
+
+/**
+ * 
+ */
+trait DataListToArrayTrait
+{
+
+    /**
+     * Returns the list data converted as an array.
+     * 
+     * @return array The list data converted as an array.
+     * @throws \InvalidArgumentException
+     */
+    public function toArray(): array
+    {
+        $this->internalDataListUpdate();
+
+        // Copied from DataObjectToArrayTrait. Do not modify here !!!
+        $toArray = function ($object) use (&$toArray) {
+            $result = [];
+
+            if ($object instanceof \ArrayObject) {
+                $vars = (array) $object; // Needed for PHP 7.4.
+                foreach ($vars as $name => $value) {
+                    $result[$name] = null;
+                }
+            } else {
+                $vars = get_object_vars($object);
+                foreach ($vars as $name => $value) {
+                    if ($name !== 'internalDataObjectData') {
+                        $reflectionProperty = new \ReflectionProperty($object, $name);
+                        if ($reflectionProperty->isPublic()) {
+                            $result[$name] = null;
+                        }
+                    }
+                }
+            }
+            if (isset($object->internalDataObjectData)) {
+                foreach ($object->internalDataObjectData['p'] as $name => $value) {
+                    $result[$name] = null;
+                }
+                foreach ($object->internalDataObjectData['d'] as $name => $value) {
+                    $result[$name] = null;
+                }
+            }
+            ksort($result);
+            foreach ($result as $name => $null) {
+                $value = $object instanceof \ArrayAccess ? $object[$name] : (isset($object->$name) ? $object->$name : null);
+                if (is_object($value)) {
+                    if (method_exists($value, 'toArray')) {
+                        $result[$name] = $value->toArray();
+                    } else {
+                        if ($value instanceof \DateTime) {
+                            $result[$name] = $value->format('c');
+                        } else {
+                            $propertyVars = $toArray($value);
+                            foreach ($propertyVars as $propertyVarName => $propertyVarValue) {
+                                if (is_object($propertyVarValue)) {
+                                    $propertyVars[$propertyVarName] = $toArray($propertyVarValue);
+                                }
+                            }
+                            $result[$name] = $propertyVars;
+                        }
+                    }
+                } else {
+                    $result[$name] = $value;
+                }
+            }
+            return $result;
+        };
+
+        $result = [];
+        foreach ($this->internalDataListData as $index => $object) {
+            $object = $this->internalDataListUpdateValueIfNeeded($this->internalDataListData, $index);
+            if (method_exists($object, 'toArray')) {
+                $result[] = $object->toArray();
+            } else {
+                $result[] = $toArray($object);
+            }
+        }
+        return $result;
+    }
+}
